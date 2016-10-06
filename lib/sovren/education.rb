172729fac7473aed58a41acbe29434c4bee2c52a
James <james@localwisejobs.com>
2016-10-06 13:59:09 -0700

    Rename education attribute name to school

--- lib/sovren/education.rb
+++ lib/sovren/education.rb
@@ -1,12 +1,12 @@
 module Sovren
   class Education
-    attr_accessor :name, :degree, :start_date, :end_date
+    attr_accessor :school, :degree, :start_date, :end_date
     
     def self.parse(education_history)
       return Array.new if education_history.nil?
       result = education_history.css('SchoolOrInstitution').collect do |item|
         education = Education.new
-        education.name = item.css('SchoolName').text
+        education.school = item.css('SchoolName').text
         education.degree = item.css('Degree DegreeName').text
         education.start_date = Date.parse(item.css('DatesOfAttendance StartDate AnyDate').text) rescue nil
         education.end_date = Date.parse(item.css('DatesOfAttendance EndDate AnyDate').text) rescue nil
